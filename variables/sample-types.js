b111021c35cc0650183e981a3870d6ad823d8990
bergabruno <brunoberga77@gmail.com>
2022-10-27 17:00:45 -0300

    add variables

---
+++ variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var number1 = 1012.6;
+var firstName = "Bruno";
+console.log(found);
+//useful to long strings
+console.log("the number is ".concat(number1));
+console.log("hi " + firstName);
---
+++ variables/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let number1: number = 1012.6;
+let firstName: string = "Bruno";
+
+console.log(found);
+//useful to long strings
+console.log(`the number is ${number1}`);
+
+
+console.log("hi " + firstName);
+
